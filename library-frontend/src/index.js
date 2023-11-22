3585d40c9c55f7fdb1adbf366d0bdd69618e92b7
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-23 00:06:16 +0500

    Install and configure apollo client

--- library-frontend/src/index.js
+++ library-frontend/src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </Router>
 );
