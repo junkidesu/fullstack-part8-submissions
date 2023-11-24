df2684c0a9826bab7c236f8e52c1874b48c8b2ad
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-24 11:54:52 +0500

    Use GraphQL in recommendations

--- library-frontend/src/components/Recommendations.js
+++ library-frontend/src/components/Recommendations.js
@@ -2,32 +2,24 @@ import { useQuery } from "@apollo/client";
 import { ALL_BOOKS, ME } from "../queries";
 
 const Recommendations = () => {
-  const booksQuery = useQuery(ALL_BOOKS);
-  const meQuery = useQuery(ME);
+  const { data: userData, loading: userLoading } = useQuery(ME);
+  const { data: booksData, loading: booksLoading } = useQuery(ALL_BOOKS, {
+    variables: {
+      genre: userData ? userData.me.favoriteGenre : undefined,
+    },
+    skip: !userData,
+  });
 
-  if (booksQuery.loading || meQuery.loading) return <div>Loading...</div>;
-
-  const books = booksQuery.data.allBooks;
-
-  const genres = [
-    ...new Set(
-      books
-        .map((b) => b.genres)
-        .flat()
-        .values()
-    ),
-  ];
-
-  const favoriteGenre = meQuery.data.me.favoriteGenre;
-  const booksToShow = books.filter((b) => b.genres.includes(favoriteGenre));
-
-  console.log(genres);
+  if (booksLoading || userLoading) return <div>Loading...</div>;
 
   return (
     <div>
       <h2>recommendations</h2>
 
-      <p>books in your favorite genre {favoriteGenre}</p>
+      <p>
+        books in your favorite genre{" "}
+        <strong>{userData.me.favoriteGenre}</strong>
+      </p>
       <table>
         <tbody>
           <tr>
@@ -35,7 +27,7 @@ const Recommendations = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {booksToShow.map((a) => (
+          {booksData.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
